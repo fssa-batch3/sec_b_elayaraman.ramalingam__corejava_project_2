bd8d2dde5d462ecd26fc2630165f1c3a232ac07f
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-09-07 16:20:54 +0530

    Update ConnectionUtil.java

--- src/main/java/in/fssa/sundaratravels/util/ConnectionUtil.java
+++ src/main/java/in/fssa/sundaratravels/util/ConnectionUtil.java
@@ -20,21 +20,6 @@ public class ConnectionUtil {
         username = System.getenv("DATABASE_USERNAME");
         password = System.getenv("DATABASE_PASSWORD");
 
-        if(url == null) {
-        	url = "jdbc:mysql://164.52.216.41:3306/elayaraman_ramalingam_corejava_project" ;
-//			url = "jdbc:mysql://127.0.0.1:3306/sundaratravels";
-        }
-
-        
-        if(username == null) {
-        	username = "cIeKvqxHC9lX" ;
-//   	 	username = "root";
-        }
-
-        if(password == null) {
-        	password = "c3fe8bfb-9dae-4aaa-a8ce-54f39d812a60" ;
-//  		password = "Elaya@007";
-        }
 
         
        
